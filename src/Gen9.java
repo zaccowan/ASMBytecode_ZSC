9af12e764e6ef33564f1de1a9d1813176f145e10
Zac Cowan <zcowan@bellarmine.edu>
2024-02-21 12:15:58 -0500

    Comment Changes

--- src/Gen9.java
+++ src/Gen9.java
@@ -48,36 +48,46 @@ public class Gen9 {
             MethodVisitor mv=cw.visitMethod(Opcodes.ACC_PUBLIC+Opcodes.ACC_STATIC, "main", "([Ljava/lang/String;)V", null, null);
             mv.visitCode();
 
+            // Initializes a new scanner
             mv.visitTypeInsn(Opcodes.NEW, "java/util/Scanner");
             mv.visitInsn(Opcodes.DUP);
 
             mv.visitFieldInsn(Opcodes.GETSTATIC, "java/lang/System", "in", "Ljava/io/InputStream;" );
             mv.visitMethodInsn(Opcodes.INVOKESPECIAL, "java/util/Scanner", "<init>", "(Ljava/io/InputStream;)V", false);
-            mv.visitVarInsn(Opcodes.ASTORE, 1);
+            mv.visitVarInsn(Opcodes.ASTORE, 1); // store the scanner object to allow future method calls from it
 
+
+            // Print prompt to user
             mv.visitFieldInsn(Opcodes.GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
             mv.visitLdcInsn("Enter a number to loop to.");
             mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/io/PrintStream", "println", "(Ljava/lang/String;)V", false);
 
+            // Load the scanner
             mv.visitVarInsn(Opcodes.ALOAD, 1);
 
+            // Make scanner call to take in integer value from user
             mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/util/Scanner", "nextInt", "()I", false);
             mv.visitVarInsn(Opcodes.ISTORE, 2);
 
+            // Print blank line for formatting
             mv.visitFieldInsn(Opcodes.GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
             mv.visitLdcInsn("");
             mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/io/PrintStream", "println", "(Ljava/lang/String;)V", false);
 
 
 
+            //
+            // Loop to print values from 0 to users integer entered above
             Label endLabel = new Label();
             Label whileStartLabel = new Label();
 
+            // initial count = 0
             mv.visitInsn(Opcodes.ICONST_0);
             mv.visitVarInsn(Opcodes.ISTORE, 3);
             mv.visitLabel(whileStartLabel);
             mv.visitVarInsn(Opcodes.ILOAD, 3);
 
+            // Greater than conditional include the value entered by the user in the loop (as compared to GE)
             mv.visitVarInsn(Opcodes.ILOAD, 2);
             mv.visitJumpInsn(Opcodes.IF_ICMPGT, endLabel);
 
